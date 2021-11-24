71cb0ee08c705a8474cbda2b4cc8faef5f4c17e1
lucieb92 <lucie.bakels@univie.ac.at>
2021-11-24 17:50:56 +0100

    moved scalev to calcpar

--- src/calcpar.f90
+++ src/calcpar.f90
@@ -760,3 +760,40 @@ subroutine richardson(psurf,ust,ttlev,qvlev,ulev,vlev,nuvz, &
   endif
 
 end subroutine richardson
+
+real function scalev(ps,t,td,stress)
+
+  !********************************************************************
+  !                                                                   *
+  !                       Author: G. WOTAWA                           *
+  !                       Date:   1994-06-27                          *
+  !                       Update: 1996-05-21 A. Stohl                 *
+  !                                                                   *
+  !********************************************************************
+  !                                                                   *
+  !     This Programm calculates scale velocity ustar from surface    *
+  !     stress and air density.                                       *
+  !                                                                   *
+  !********************************************************************
+  !                                                                   *
+  !     INPUT:                                                        *
+  !                                                                   *
+  !     ps      surface pressure [Pa]                                 *
+  !     t       surface temperature [K]                               *
+  !     td      surface dew point [K]                                 *
+  !     stress  surface stress [N/m2]                                 *
+  !                                                                   *
+  !********************************************************************
+  
+  use par_mod
+
+  implicit none
+
+  real :: ps,t,td,e,ew,tv,rhoa,stress
+
+  e=ew(td,ps)                       ! vapor pressure
+  tv=t*(1.+0.378*e/ps)           ! virtual temperature
+  rhoa=ps/(r_air*tv)              ! air density
+  scalev=sqrt(abs(stress)/rhoa)
+
+end function scalev

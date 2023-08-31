4e35a12e16bc70d6d6c381b1a4ef0bb2d908fd8a
lucieb92 <lucie.bakels@univie.ac.at>
2023-08-31 12:17:12 +0200

    Added Python script from openmp test
    Former-commit-id: 23e0e5f3a6affc9055abf958e5208c1389a5e75b

---
+++ tests/openmp_test.py
@@ -0,0 +1,21 @@
+import netCDF4 as nc
+import numpy as np
+
+ndata_1 = nc.Dataset("./output_omp1/partoutput_20090101000000_init.nc", 'r')
+ndata_32 = nc.Dataset("./output_omp32/partoutput_20090101000000_init.nc", 'r')
+
+dvars = list(ndata_1.variables)
+for idvars in dvars:
+	temp = ndata_1[idvars][:]-ndata_32[idvars][:]
+	print(idvars, ':')
+	mn=np.mean(np.abs(temp))
+	mx=np.max(np.abs(temp))
+	mean1=(np.mean(np.abs(ndata_1[idvars])))
+	print("mean difference:",mn," max abs difference:",mx, "mean: ", mean1)
+
+	print("small mean", smallmean)
+	if (mn>(mean1/1.e8)):
+		print('Mean difference exceeds allowed value of ',mean1/1.e8)
+		return 1
+ndata_1.close()
+ndata_32.close()

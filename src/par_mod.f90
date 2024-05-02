97146fc0a889cdc22cc5036d2b4b54f2a6e1053a
lucieb92 <lucie.bakels@univie.ac.at>
2024-05-02 10:19:27 +0200

    set incloud_ratio to old parameter
    Former-commit-id: 57c4a67b475faf1c8b16ddf8591f591e355e3670

--- src/par_mod.f90
+++ src/par_mod.f90
@@ -80,7 +80,7 @@ module par_mod
   !real,parameter :: d_trop=50., d_strat=0.1
   real :: d_trop=50., d_strat=0.1, fturbmeso=0.16 ! turbulence factors can change for different runs
   real,parameter :: rho_water=1000. !ZHG 2015 [kg/m3]
-  real,parameter :: ratio_incloud=0.1   !MC 2024
+  real,parameter :: ratio_incloud=0.0062   !MC 2024
   real,parameter :: wet_a=1.e-5, wet_b=0.8 !AT
 
   ! karman            Karman's constant
